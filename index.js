3c80adfd9f629a69854536e191501cecc6adc7c4
Tim <117385556+timpel301@users.noreply.github.com>
2023-10-29 10:42:22 +0100

    feat(src): finished implementation

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer  = require('multer');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 var app = express();
 
+const uploadSchema = new mongoose.Schema({
+name: String,
+data: Buffer,
+type: String,
+size: Number,
+});
+
+const Upload = mongoose.model('Upload', uploadSchema);
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+
+  const upload = new Upload();
+  upload.name = req.file.originalname;
+  upload.data = req.file.buffer;
+  upload.type = req.file.mimetype;
+  upload.size = req.file.size;
+
+  upload.save();
+
+  res.json({name: upload.name, type: upload.type, size: upload.size})
+});
 
 
 const port = process.env.PORT || 3000;
